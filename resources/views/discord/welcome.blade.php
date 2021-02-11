c1faabd0e96fc3372650dfd98054a5d5358103b2
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2021-02-11 19:43:37 +0100

    rough block-out of ui

---
+++ resources/views/discord/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Discord</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased">
+            <main>
+                <div class="flex flex-row h-screen">
+                    <div class="w-16 bg-red-200">primary</div>
+                    <div class="w-64 bg-green-200">second</div>
+                    <div class="flex-auto bg-blue-200">chat box</div>
+                </div>
+            </main>
+        </div>
+
+        @livewireScripts
+    </body>
+</html>
+
