edeb0bea5c0fa8ef8a97a582b31d680e87f65a7a
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2021-02-12 16:27:25 +0100

    rounded and added shadow

--- resources/views/discord/welcome.blade.php
+++ resources/views/discord/welcome.blade.php
@@ -20,10 +20,43 @@
     </head>
     <body class="font-sans antialiased">
             <main>
-                <div class="flex flex-row h-screen">
-                    <div class="w-16 bg-red-200">primary</div>
-                    <div class="w-64 bg-green-200">second</div>
-                    <div class="flex-auto bg-blue-200">chat box</div>
+                <div class="flex flex-row h-screen bg-gray-200">
+                    <div class="w-16 bg-gray-800 flex flex-col justify-between items-center p-4 ">
+                        <div class="flex flex-col space-y-2">
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                        </div>
+                        <div class="flex flex-col space-y-2">
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+                            <a> <div class="rounded-full bg-gray-200 w-8 h-8"></div> </a>
+
+                        </div>
+                    </div>
+                    <div class="w-64 bg-gray-200 p-4 space-y-2">
+                        <div class="flex flex-row justify-between items-center">
+                            <h1>Channel Name</h1>
+                            <svg class="w-3 h-3 flex-none" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+                        </div>
+                        <div class="flex flex-col bg-red-300 h-64">
+                            <svg class="w-3 h-3 flex-none" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+                            <p class="">GENERAL</p>
+                            <div class="bg-red-400">
+                                #chat
+                            </div>
+                        </div>
+                        <div class="flex flex-row justify-between items-center bg-red-300 h-64">
+                            
+                        </div>
+                        <div class="bg-red-300 h-64"></div>
+                    </div>
+                    <div class="flex-auto bg-blue-200 rounded-tl-xl shadow-2xl ">chat box</div>
                 </div>
             </main>
         </div>
